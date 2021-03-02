a0609f28f2a8a04affdf6f2d6906eda199a8fb11
hanstjandra68 <c14180068@john.petra.ac.id>
2021-03-02 18:04:59 +0700

    2 files modified, 4 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Hal2Component } from './hal2/hal2.component';
+import { Hal3Component } from './hal3/hal3.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, Hal2Component, Hal3Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
