126ed16f82162f2a48f21084926a912b067c63cf
hanstjandra68 <c14180068@john.petra.ac.id>
2021-03-02 17:57:12 +0700

    Initial commit

---
+++ src/hal1/hal1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hal1',
+  templateUrl: './hal1.component.html',
+  styleUrls: ['./hal1.component.css']
+})
+export class Hal1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
